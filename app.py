d633833a3f1dbf8215395c655a3983e1172005c8
Adittii72 <1by23cs009@bmsit.in>
2026-05-22 22:35:02 +0530

    Added timeline

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import preprocessor, helper
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.sidebar.title("WhatsApp Chat Analysis")
 
@@ -10,7 +11,6 @@ if uploaded_file is not None:
   data = bytes_data.decode("utf-8")
   df = preprocessor.preprocess(data)
 
-  st.dataframe(df)
 
   users_list = df['user'].unique().tolist()
   users_list.remove('group_notification')
@@ -21,8 +21,8 @@ if uploaded_file is not None:
 
   if st.sidebar.button("Show Analysis"):
     num_messages, words, num_media_messages, links = helper.fetch_stats(selected_user, df)
+    st.title("Top Statistics")
     col1, col2, col3, col4 = st.columns(4)
-
     with col1:
       st.metric("Total Messages", num_messages)
 
@@ -34,6 +34,48 @@ if uploaded_file is not None:
 
     with col4:
       st.metric("Links Shared", links)
+
+    #Timeline
+    timeline = helper.monthly_timeline(selected_user, df)
+    st.title("Monthly Timeline")
+    fig, ax = plt.subplots()
+    ax.plot(timeline['time'], timeline['message'], color='blue')
+    plt.xticks(rotation='vertical')
+    st.pyplot(fig)
+
+    timeline = helper.daily_timeline(selected_user, df)
+    st.title("Daily Timeline")
+    fig, ax = plt.subplots()
+    ax.plot(timeline['only_date'], timeline['message'], color='blue')
+    plt.xticks(rotation='vertical')
+    st.pyplot(fig)
+
+    st.title("Activity Map")
+    col1, col2 = st.columns(2)
+
+    with col1:
+      st.header("Most Busy Day")
+      busy_day = helper.week_activity_map(selected_user, df)
+      fig, ax = plt.subplots()
+      ax.bar(busy_day.index, busy_day.values, color='pink') 
+      plt.xticks(rotation='vertical')
+      st.pyplot(fig)
+
+
+    with col2:
+      st.header("Most Busy Month")
+      busy_month = helper.month_activity_map(selected_user, df)
+      fig, ax = plt.subplots()
+      ax.bar(busy_month.index, busy_month.values, color='pink') 
+      plt.xticks(rotation='vertical')
+      st.pyplot(fig)
+
+
+    st.title("Activity Heatmap")
+    user_heatmap = helper.activity_heatmap(selected_user, df)
+    fig, ax = plt.subplots()
+    ax = sns.heatmap(user_heatmap)
+    st.pyplot(fig)
 
 
     if selected_user=='Overall':
